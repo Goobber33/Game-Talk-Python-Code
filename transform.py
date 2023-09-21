40123fa95158b4498e59487a7b8c5665a8bd56dd
Kyle <kcparks1234@gmail.com>
2023-09-20 18:32:10 -0700

    Added initial code

--- transform.py
+++ transform.py
@@ -1,27 +1,35 @@
 from pymongo import MongoClient
 import json
+import os
+from dotenv import load_dotenv
 
-# load your data
+# Load environment variables
+load_dotenv()
+
+# Fetch MongoDB credentials and database name from .env
+username = os.environ.get('MONGO_USERNAME')
+password = os.environ.get('MONGO_PASSWORD')
+dbname = os.environ.get('MONGO_DBNAME')
+
+# Load your data
 with open('transformed_data.json') as f:
     data_dict = json.load(f)
 
-# flatten the data into a list of dictionaries
+# Flatten the data into a list of dictionaries
 data = []
 for game, terms in data_dict.items():
     for term in terms:
         term["game"] = game  # add game name to each term document
         data.append(term)
 
-# replace the placeholder data with your actual username, password, and dbname
-username = "kcparks1234"
-password = "TTs2JytYedkzoOpg"
-dbname = "gdata"
+# Create MongoDB client connection
 client = MongoClient(f"mongodb+srv://{username}:{password}@cluster0.eqfzrq7.mongodb.net/{dbname}?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true")
 
+# Access the database
 db = client[dbname]
 
-# replace 'your_collection' with your actual collection name
+# Replace 'your_collection' with your actual collection name
 collection = db['gdata']
 
-# insert the flattened data into the collection
+# Insert the flattened data into the collection
 collection.insert_many(data)
